d4009395c74a5efaf64c713f1070ae57f400e41a
root <root@LAPTOP-NFTP3RDA>
2023-08-25 18:29:07 +0200

    create card

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
 
-
-
 const endAndPoint = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en'
-
 async function getCrypto() {
     await fetch(`${endAndPoint}`)
         .then(response => response.json())
-        .then(cryptoDati => {
-          cryptoDati
-        })
+        .then(cryptoDati => render(cryptoDati))
 }
+getCrypto()
 
-function render(){
-    const div : HTMLElement = document.querySelector('div') as HTMLElement;
+function render(dati: any[]) {
+    const title: HTMLElement = document.querySelector('div') as HTMLElement
+    const card = document.createElement('div');
 
+    dati.map((cpt) => {
+        card.classList.add('card', cpt.name);
+        card.insertAdjacentHTML('afterbegin', `
+        <h3>${cpt.name} - ${cpt.symbol}</h3>
+        <img src="${cpt.image} height="50px" width="50px" />
+        <p><strong>Valore attuale:</strong>${cpt.current_price}</p>
+        <p><strong>Min 24h:</strong>${cpt.low_24h} - <strong>Max 24h</strong>${cpt.high_24h}</p>
+        `)
+title.insertAdjacentElement('afterend', card)
 
+    })
 }
